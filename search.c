f330138c0ce616d9bcf644faccad51b2fc339166
Kim gyeong rock <rudfhr97@naver.com>
2020-05-13 04:38:58 +0900

    find bug

--- search.c
+++ search.c
@@ -4,7 +4,8 @@ void searchBookName(Book *b, int count){
 	int bcount = 0;
 	char search[100];
 	printf("검색할도서의 이름은? ");
-	scanf("%s", search);
+	scanf("%[^/n]s", search);
+	search[strlen(search)-1]='\0';
 	for(int i =0; i<count; i++){
 		if(strstr(b[i].name, search)){
 			readBook(b[i]);
