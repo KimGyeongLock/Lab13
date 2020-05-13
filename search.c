2af9ef5d243a9339573ee9aab02122700a235f2e
breezebee <bluebell8400@gmail.com>
2020-05-13 12:16:55 +0900

    Fix search function bug

--- search.c
+++ search.c
@@ -4,7 +4,8 @@ void searchBookName(Book *b, int count){
 	int bcount = 0;
 	char search[100];
 	printf("검색할도서의 이름은? ");
-	scanf("%[^/n]s", search);
+	getchar();
+	fgets(search, 100, stdin);
 	search[strlen(search)-1]='\0';
 	for(int i =0; i<count; i++){
 		if(strstr(b[i].name, search)){
